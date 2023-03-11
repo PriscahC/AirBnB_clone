1a249f70620051d72119c8b2d65f5517fc9453a6
robert5313 <kiptoorobert@zetech.ac.ke>
2023-03-11 16:12:52 +0300

    adding the test modules

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from datetime import datetime
+from uuid import uuid4
+ 
+
+class BaseModel(object):
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
+    
