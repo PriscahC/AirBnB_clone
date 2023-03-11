4b83ac4fae0cfe97fb7c439ca531081659688fd3
robert5313 <kiptoorobert@zetech.ac.ke>
2023-03-11 16:33:14 +0300

    adding the test modules

--- models/base_model.py
+++ models/base_model.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python
+"""BaseModel Module for AirBnB Clone Project"""
 
 from datetime import datetime
 from uuid import uuid4
  
 
 class BaseModel(object):
+    """BaseModel Class for AirBnB Clone Project"""
     def __init__(self, *args, **kwargs):
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """Returns a string representation of the BaseModel instance"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
+        """updated_at' attribute with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
         return my_dict
-    
