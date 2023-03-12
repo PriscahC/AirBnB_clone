68de35b8c31ff808de4b0c07306aa073b9420d8c
robert5313 <kiptoorobert@zetech.ac.ke>
2023-03-12 16:54:09 +0300

    the user modules

---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module creates the state class that inherits from BaseModel class"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """State class"""
+    
+    name = ""
