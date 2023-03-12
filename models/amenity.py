9ca60f364848bff67a765e2c59f147d389ac1a48
Priscila A <105573463+PriscahC@users.noreply.github.com>
2023-03-12 16:22:58 -0700

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -5,8 +5,6 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     """Class for managing amenity objects
-    Represent an amenity.
-    Attributes:
     name (str): The name of the amenity.
     """
     name = ""
